80caa0254d2dcfcfcdc16c386b84f59ad11bcf9e
Penny Hanson <centavoconsulting@gmail.com>
2019-01-28 10:49:02 +0000

    Add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectId} = require('mongodb');
 var { mongoose } = require('./db/mongoose.js');
 var { Todo } = require('./models/todo.js');
 var { User } = require('./models/user.js');
@@ -27,6 +28,21 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+            
+    if (!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    };
+    Todo.findById(id).then((todo) => {
+    if (!todo) {
+        return res.status(404).send();
+    };
+        res.send({todo});
+}).catch((e) => res.status(400).send());
+    
 });
 
 app.listen(3000, () => {
