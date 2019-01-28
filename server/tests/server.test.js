e1446b1a37c4a2265bdbd0e8e247fb64a286b1da
Penny Hanson <centavoconsulting@gmail.com>
2019-01-28 17:19:41 +0000

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -96,3 +96,37 @@ describe('GET /todos/:id', () => {
     });
     
 });
+
+describe('DELETE /todos/:id', (done) => {
+    it('should remove a todo', (done) => {
+        var hexID = todos[1]._id.toHexString();
+
+        request(app)
+        .delete(`/todos/${hexID}`)
+        .expect(200)
+        .expect ((res) => {
+            expect (res.body.todo._id).toBe(hexID)
+        }).end((err, res) => {
+            if(err) {
+                return done(err);
+            }
+            Todo.findById(hexID).then((todo) => {
+                expect(todo).toNotExist();
+                done();
+            }).catch((e) => done(e));
+        })
+    });
+    it('should return 404 if todo not found', (done) => {
+        var id = new ObjectID();
+        request(app)
+        .delete(`/todos/${id.toHexString()}`)
+        .expect(404)
+        .end(done);
+    });
+    it('should return 404 if object id is invalid', (done) => {
+        request(app)
+        .delete('/todos/123')
+        .expect(404)
+        .end(done);
+    });
+})
